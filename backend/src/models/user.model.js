f8e05c9510cccbc7aff7ac318879b28960ff993c
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-05 23:34:27 +0500

    Refactor server initialization to enhance error handling during database connection and improve logging for better debugging.

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    currentField: {
+      type: String,
+      default: "",
+    },
+    learningField: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    isOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+//pre hook : just before saving the user in the database we need to hash the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    //salt : random string of characters that is used to hash the password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    //next is used to move to the next middleware
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default User;
