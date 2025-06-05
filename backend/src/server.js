d1f13b25badc00ec485829991269c5320243afb3
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-05 23:10:47 +0500

    Integrate database connection in server startup by importing and invoking connectDB function.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./db/db.js";
 dotenv.config();
 
 const app = express();
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
