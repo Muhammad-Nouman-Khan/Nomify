04c5ea7bb3238987f619d7f335294ad48aecc26c
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-07 17:44:22 +0500

    Integrate lucide-react for icons in SignUp component, enhance authentication flow with conditional routing, and remove unused vite.svg file. Update package dependencies in package.json and package-lock.json.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
@@ -13,26 +13,50 @@ import { axiosInstance } from "./lib/axios";
 const App = () => {
   //tanstack query : used to fetch data from the server
   // mutation : used to create, update, delete data from the server
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["todos"],
+  const {
+    data: authData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["authUser"],
     queryFn: async () => {
       const res = await axiosInstance.get("/auth/me");
       return res.data;
     },
     retry: false, // auth check
   });
-  console.log(data);
-
+  const authUser = authData?.user; //here its user because we are returning user from this endpoint
   return (
     <div className=" h-screen" data-theme="night">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/call" element={<Call />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="signup"
+          element={!authUser ? <SignUp /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <Onboarding /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <Notifications /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/call"
+          element={authUser ? <Call /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <Chat /> : <Navigate to="/login" />}
+        />
       </Routes>
       <Toaster />
     </div>
