a91086568b450ab26fdcf42a0b5c8d2ca65298c2
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-09 18:55:02 +0500

    Update backend and frontend dependencies, refactor server setup for production, and enhance HTML structure. Adjust package versions for express, mongoose, and stream-chat. Add start script to package.json and improve axios configuration for dynamic base URL.

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,14 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./db/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -27,6 +31,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 // it is used to create a new instance of axios with a base URL
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
   withCredentials: true, //send cookies with the request
 });
